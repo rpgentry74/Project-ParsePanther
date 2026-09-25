b7711f21f21a63b5819a13634e8fbecd178e015c
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 15:38:07 -0700

    Derive display and cache channels from the app version

--- appConfig.js
+++ appConfig.js
@@ -4,12 +4,16 @@ export const APP_VERSION = '3.0.0-dev';
 export const BUILD_NUMBER = '25';
 export const IS_DEVELOPMENT = APP_VERSION.endsWith('-dev');
 
-const CACHE_CHANNEL = IS_DEVELOPMENT ? 'v3-dev' : 'v3';
+const MAJOR_VERSION = APP_VERSION.split('.')[0];
+const CACHE_CHANNEL = IS_DEVELOPMENT
+  ? `v${MAJOR_VERSION}-dev`
+  : `v${MAJOR_VERSION}`;
+
 export const CACHE_NAME =
   `parsepanther-${CACHE_CHANNEL}-${BUILD_NUMBER}`;
 
 export const DISPLAY_VERSION = IS_DEVELOPMENT
-  ? 'v3.0 Development'
+  ? `v${APP_VERSION.replace(/-dev$/, '')} Development`
   : `v${APP_VERSION}`;
 
 export function applyBuildMetadata() {
