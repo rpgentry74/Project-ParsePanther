751e50338919eb18a6e6ca72ef63cd4317a87139
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 15:35:45 -0700

    Derive cache channel from central release version

--- appConfig.js
+++ appConfig.js
@@ -2,8 +2,11 @@
 
 export const APP_VERSION = '3.0.0-dev';
 export const BUILD_NUMBER = '25';
-export const CACHE_NAME = `parsepanther-v3-dev-${BUILD_NUMBER}`;
 export const IS_DEVELOPMENT = APP_VERSION.endsWith('-dev');
+
+const CACHE_CHANNEL = IS_DEVELOPMENT ? 'v3-dev' : 'v3';
+export const CACHE_NAME =
+  `parsepanther-${CACHE_CHANNEL}-${BUILD_NUMBER}`;
 
 export const DISPLAY_VERSION = IS_DEVELOPMENT
   ? 'v3.0 Development'
