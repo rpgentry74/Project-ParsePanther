f2d10000d6471497d0e21674478c4f6ed30ee50a
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 12:37:15 -0700

    Refresh v3 cache for indirect evidence status separation

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 const CACHE_PREFIX = 'parsepanther-';
-const CACHE_NAME = 'parsepanther-v3-dev-16';
+const CACHE_NAME = 'parsepanther-v3-dev-17';
 const LEGACY_CACHE_NAMES = new Set(['v1']);
 
 const APP_SHELL = [
