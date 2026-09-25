03363a6ff855916cf6e0be0113405576bfc74fc1
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 11:11:52 -0700

    Bypass browser cache when installing v3 app shell

--- service-worker.js
+++ service-worker.js
@@ -45,7 +45,13 @@ const APP_SHELL = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_SHELL))
+      .then((cache) =>
+        Promise.all(
+          APP_SHELL.map((url) =>
+            cache.add(new Request(url, { cache: 'reload' }))
+          )
+        )
+      )
       .then(() => self.skipWaiting())
   );
 });
