b9e5b4f083b98b9befac45427500a36520e6337f
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 12:00:59 -0700

    Refresh v3 cache for empty Direct prerequisite confirmation flow

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 const CACHE_PREFIX = 'parsepanther-';
-const CACHE_NAME = 'parsepanther-v3-dev-14';
+const CACHE_NAME = 'parsepanther-v3-dev-15';
 const LEGACY_CACHE_NAMES = new Set(['v1']);
 
 const APP_SHELL = [
