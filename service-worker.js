3a752648cc00279c9a093dc5cd4968761f676b03
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 15:17:22 -0700

    Refresh v3 cache after clarity refinements

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 const CACHE_PREFIX = 'parsepanther-';
-const CACHE_NAME = 'parsepanther-v3-dev-23';
+const CACHE_NAME = 'parsepanther-v3-dev-24';
 const LEGACY_CACHE_NAMES = new Set(['v1']);
 
 const APP_SHELL = [
