91208dc2a3eeba857e85b2b99bd04de886bb4482
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 17:07:17 -0700

    Advance development build after wide-table finalization

--- appConfig.js
+++ appConfig.js
@@ -1,7 +1,7 @@
 // appConfig.js
 
 export const APP_VERSION = '3.0.0-dev';
-export const BUILD_NUMBER = '27';
+export const BUILD_NUMBER = '28';
 export const IS_DEVELOPMENT = APP_VERSION.endsWith('-dev');
 
 const MAJOR_VERSION = APP_VERSION.split('.')[0];
