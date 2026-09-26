ad947f79300a64f87f8cba7c8f70c78104827aa7
Richard  Gentry <gentryr@scc.losrios.edu>
2026-09-25 18:10:18 -0700

    Promote Version 3 to production release

--- appConfig.js
+++ appConfig.js
@@ -1,7 +1,7 @@
 // appConfig.js
 
-export const APP_VERSION = '3.0.0-dev';
-export const BUILD_NUMBER = '32';
+export const APP_VERSION = '3.0.0';
+export const BUILD_NUMBER = '33';
 export const IS_DEVELOPMENT = APP_VERSION.endsWith('-dev');
 
 const MAJOR_VERSION = APP_VERSION.split('.')[0];
